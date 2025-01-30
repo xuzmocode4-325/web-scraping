5c58038e9b7be3ca86b6178f6ae31d7d1b07167d
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-01-30 03:52:15 +0200

    update crawl spider

--- getting_started/getting_started/spiders/transcripts.py
+++ getting_started/getting_started/spiders/transcripts.py
@@ -5,16 +5,24 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class TranscriptsSpider(CrawlSpider):
     name = "transcripts"
-    allowed_domains = ["subslikescripts.com"]
-    start_urls = ["https://subslikescripts.com/movies"]
+    allowed_domains = ["subslikescript.com"]
+    start_urls = ["https://subslikescript.com/movies"]
+
+    custom_settings = {
+        'DOWNLOAD_DELAY': 0.5
+    }
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths=()), callback="parse_item", follow=True),
+        Rule(LinkExtractor(restrict_xpaths=('//ul[contains(@class, "scripts-list")]//li/a')), callback="parse_item", follow=True),
+        Rule(LinkExtractor(restrict_xpaths=('//ul[@class="pagination"]/li/a[@rel="next"]')), follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
-        #item["name"] = response.xpath('//div[@id="name"]').get()
-        #item["description"] = response.xpath('//div[@id="description"]').get()
-        return item
+        article = response.xpath('//article[@class="main-article"]')
+        yield {
+            'title' : article.xpath('./h1').get(), 
+            'plot': article.xpath('./p').get(),
+            'url': response.url
+            #'transcript': article.xpath('./div[@class="full-script"]//p[@class="cue-line"]/text()').getall(),
+        }
+        # print(article.xpath('./div[@class="full-script"]//p[@class="cue-line"]/text()').getall())
